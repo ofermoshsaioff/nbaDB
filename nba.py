ca5d9d67487aff18b75c420d915aa50d50079f24
Ofer Moshaioff <ofer.moshaioff@gmail.com>
2013-12-19 12:30:08 +0200

    add general stats and fixed #2 and fixed #1

--- nba.py
+++ nba.py
@@ -17,11 +17,11 @@ else:
 	db = client[DATABASE]
 	games = db[COLLECTION]
 
-def insert_doc(doc, event_id):
-    name = json.dumps(doc['display_name'])
-    doc['id'] = event_id + '_' + name # adding a unique identifer: a concat of the event_id and the player's name, for example: 20131029-orlando-magic-at-indiana-pacers_Paul George
-    print('Creating a record for', doc['id'])
-    games.insert(doc)
+def insert_doc(doc, general_stats):
+	for k in general_stats:
+		doc[k] = general_stats[k]
+	print('Creating a record for %s for game %s' % (doc['display_name'], doc['event_id']))
+	games.insert(doc)
 
 def process_day(date):
 	# step 1 - get all the events from the given date
@@ -46,23 +46,31 @@ def process_day(date):
 			req = requests.get(url, headers=HEADERS)
 			bs = json.loads(req.text, 'utf-8')
 
+			# get general stats to store with each record
+			general_stats = {}
+			general_stats['event_id'] = event_id
+			general_stats['type'] = bs['event_information']['season_type']
+			general_stats['date'] = bs['event_information']['start_date_time']
+			general_stats['home_team'] = bs['home_team']['team_id']
+			general_stats['away_team'] = bs['away_team']['team_id']
+
 			# step 3 - crop home/away stats and insert all the documents to the DB
 			away = bs['away_stats']
 			home = bs['home_stats']
 
-		  	for h,a in zip(home, away):
+			for h,a in zip(home, away):
 				try:
-					insert_doc(h, event_id)
-					insert_doc(a, event_id)
+					insert_doc(h, general_stats)
+					insert_doc(a, general_stats)
 				except Exception as err:
-						print("Error inserting record to DB:", str(err))
-						continue
+					print("Error inserting record to DB:", str(err))
+					continue
 		except:
-		  	print('Error getting boxscore details for event', str(e))
-		  	continue
+			print('Error getting boxscore details for event', str(e))
+			continue
 		finally:
-		  	print('Sleeping for 10 seconds...')
-		  	time.sleep(10) # max 6 calls per minute allowed
+			print('Sleeping for 10 seconds...')
+			time.sleep(10) # max 6 calls per minute allowed
 
 # get a year from the user in YYYY format. TODO - add error handling
 year = int(sys.argv[1])
@@ -76,21 +84,21 @@ one_day = datetime.timedelta(days=1)
 
 next_day = begin_year
 for day in range(0, 366):  # includes potential leap year
-    if next_day > end_year:
-        break
-    try:
-      	day_str = next_day.strftime(DATE_FORMAT)
-      	print('Loading data for day', day_str)
-      	process_day(day_str)
-    except KeyboardInterrupt:
-    	print('User interrupt')
-    	break
-    except:
-      	print('Error processing day', day_str)
-      	continue
-    finally:
-      	# increment date object by one day
-      	next_day += one_day
+	if next_day > end_year:
+		break
+	try:
+		day_str = next_day.strftime(DATE_FORMAT)
+		print('Loading data for day', day_str)
+		process_day(day_str)
+	except KeyboardInterrupt:
+		print('User interrupt')
+		break
+	except:
+		print('Error processing day', day_str)
+		continue
+	finally:
+		# increment date object by one day
+		next_day += one_day
 
 if MOCK_DB:
 	import cPickle as pickle
