aba6228d240af501b99c4acde772b7b4f3690c7a
Ofer Moshaioff <ofer.moshaioff@gmail.com>
2013-12-28 20:58:48 +0200

    removed useless code

--- analyzer.py
+++ analyzer.py
@@ -1,5 +1,4 @@
 import math
-import numpy as np
 from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
 from pymongo import MongoClient
@@ -18,7 +17,7 @@ y_axis = []
 for p in season.find():
   stats = p['value']
   filtered_stats = {k : stats[k] for k in params } #filter the important params
-  labels.append(p['_id']) # keep plater names to use as labels for graph
+  labels.append(p['_id']) # keep player names to use as labels for graph
   data.append(filtered_stats.values())
 
 # reduce len(params) to n_components dimention
@@ -26,19 +25,12 @@ pca = PCA(n_components=2)
 pca_data = pca.fit_transform(data)
 
 # draw the results
-for i,d in enumerate(pca_data):
+for d in pca_data:
   x_axis.append(d[0])
   y_axis.append(d[1])
 
 plt.scatter(x_axis, y_axis)
 for label, x, y in zip(labels, x_axis, y_axis):
-  plt.annotate(
-    label,
-    xy = (x, y))
+  plt.annotate(label, xy = (x, y))
+
 plt.show()
-
-
-
-
-
-
